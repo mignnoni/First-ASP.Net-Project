4b73be26f86a5ac537d625b403809850fb0e16ae
mignnoni <iurimig@gmail.com>
2022-06-15 00:26:46 -0300

    Add project files.

---
+++ APIWeb/Migrations/20220615031556_heroesbattles.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APIWeb.Migrations
+{
+    public partial class heroesbattles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Heroes_Battles_BattleId",
+                table: "Heroes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Heroes_BattleId",
+                table: "Heroes");
+
+            migrationBuilder.DropColumn(
+                name: "BattleId",
+                table: "Heroes");
+
+            migrationBuilder.CreateTable(
+                name: "HeroesBattles",
+                columns: table => new
+                {
+                    HeroId = table.Column<int>(nullable: false),
+                    BattleId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HeroesBattles", x => new { x.BattleId, x.HeroId });
+                    table.ForeignKey(
+                        name: "FK_HeroesBattles_Battles_BattleId",
+                        column: x => x.BattleId,
+                        principalTable: "Battles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HeroesBattles_Heroes_HeroId",
+                        column: x => x.HeroId,
+                        principalTable: "Heroes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SecretIdentities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    RealName = table.Column<string>(nullable: true),
+                    HeroId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SecretIdentities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SecretIdentities_Heroes_HeroId",
+                        column: x => x.HeroId,
+                        principalTable: "Heroes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HeroesBattles_HeroId",
+                table: "HeroesBattles",
+                column: "HeroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SecretIdentities_HeroId",
+                table: "SecretIdentities",
+                column: "HeroId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HeroesBattles");
+
+            migrationBuilder.DropTable(
+                name: "SecretIdentities");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BattleId",
+                table: "Heroes",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Heroes_BattleId",
+                table: "Heroes",
+                column: "BattleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Heroes_Battles_BattleId",
+                table: "Heroes",
+                column: "BattleId",
+                principalTable: "Battles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ APIWeb/Models/Hero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIWeb.Models
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public SecretIdentity SecretIdentity { get; set; }
+        public List<Weapon> Weapons { get; set; }
+        public List<HeroBattle> HeroesBattles { get; set; }
+
+    }
+}
